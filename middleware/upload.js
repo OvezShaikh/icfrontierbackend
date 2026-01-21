7c1016a8f810608988b0757b4a6bab50755a9b94
Ovez <ovezshaikh13052001@gmail.com>
2026-01-21 16:50:07 +0530

    corrected upload and multer sync logic

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,15 +1,5 @@
 import multer from "multer";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "../utils/cloudinary.js";
 
-const storage = new CloudinaryStorage({
-  cloudinary,
-  params: {
-    folder: "blog-images",
-    allowed_formats: ["jpg", "png", "jpeg", "webp"],
-  },
-});
-
-const upload = multer({ storage });
-
+const storage = multer.memoryStorage();
+const upload = multer({ storage, limits: { fileSize: 5 * 1024 * 1024 } });
 export default upload;
